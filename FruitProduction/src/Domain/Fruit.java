338cbed01566e3a30ac49e0f8c1d9f8bbe356340
Pedro Monteiro - 1211076 <1211076@isep.ipp.pt>
2022-10-03 22:00:42 +0100

    Eliminação da pasta src Solução ao problema do src Again

---
+++ FruitProduction/src/Domain/Fruit.java
@@ -0,0 +1,28 @@
+package Domain;
+
+
+import java.util.Objects;
+
+public class Fruit {
+
+    private final String fruitName;
+
+    public Fruit(String fruitName) {
+        this.fruitName = fruitName;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Fruit fruit = (Fruit) o;
+        return fruitName.equals(fruit.fruitName);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(fruitName);
+    }
+
+
+}
