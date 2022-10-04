b7725efbaf1df67367924effc3bedd4f534c781c
GuilhermeSousa03 <1211073@isep.ipp.pt>
2022-10-04 12:27:01 +0100

    Implemented method that filtrate wich countrys overtake the productio quatity Q in ordered form. Beggan studying HeapSort algorithm.

--- FruitProduction/src/Utils/HeapSort.java
+++ FruitProduction/src/Utils/HeapSort.java
@@ -1,4 +1,23 @@
 package Utils;
 
-public class HeapSort {
-}
+import Controllers.App;
+import Domain.Country;
+import Domain.Fruit;
+import Domain.Quantity;
+import Domain.Year;
+import Stores.DataStore;
+
+import java.util.Map;
+
+public class HeapSort implements ISorter {
+
+    private final DataStore dataStore = App.getInstance().getStore();
+    @Override
+    public void sort() {
+        Map<Fruit, Map<Country, Map<Year, Quantity>>> fruitCountryYearQuantity = dataStore.getFruitHarvest();
+
+                for (Map<Year, Quantity> quantity : fruitCountryYearQuantity.get(0).values()) {
+
+                }
+            }
+        }
